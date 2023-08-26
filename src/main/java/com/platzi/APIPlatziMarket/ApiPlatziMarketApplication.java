6e4c3c6520b5d7f0bea07293731fca3886100b2e
Aldahir Mayorga <samg.25sep@gmail.com>
2023-08-26 14:11:28 -0600

    Inicializando Proyecto y estructura, se agregan perfiles de configuracion para entornos dev y produccion(pdn)

---
+++ src/main/java/com/platzi/APIPlatziMarket/ApiPlatziMarketApplication.java
@@ -0,0 +1,14 @@
+package com.platzi.APIPlatziMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlatziMarketApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/platzi/APIPlatziMarket/ApiPlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.APIPlatziMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
